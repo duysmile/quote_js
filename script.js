6aaa9af6b6fbde1fb4871ab06b6b4fa7c9b25a77
duy21 <duy210697@gmail.com>
2018-05-27 14:36:56 +0700

    finish quote

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function animation(text){
+    $('#quote-text').animate({
+        opacity: 0,
+    }, 500, function(){
+        $(this).animate({
+            opacity: 1,
+        }, 500);
+        $("#quote").text(text.quote);
+        $("#author").text(text.author);
+    });
+}
 function getQuote(){
     $.ajax({
         headers: {
@@ -9,14 +20,41 @@ function getQuote(){
         type: "get",
         dataType: "json",
         success: function(result){
-            $("#quote").text(result[0].quote);
-            $("#author").text(result[0].author);
+            animation(result[0]);
         },
         
     });
 }
+
+var colors = [
+    '#16a085', 
+    '#27ae60', 
+    '#2c3e50', 
+    '#f39c12', 
+    '#e74c3c', 
+    '#9b59b6', 
+    '#FB6964', 
+    '#342224', 
+    "#472E32", 
+    "#BDBB99", 
+    "#77B1A9", 
+    "#73A857"
+];
+
+function getColor(){
+    var color = Math.floor(Math.random() * 12);
+    color = colors[color];
+    $('body, .btn').css({
+        "transition": "all 1.5s",
+        "background": color,  
+    });
+}
+
 $(document).ready(function(){
+    getQuote();
+    getColor();
     $("#change_quote").on('click', function(){
+        getColor();
         getQuote();
     });
 });
